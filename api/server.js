af7a5e6ec95c7b2f02ed07e1315948bb3604fab0
Gretchyn Hickman <82916149+gretchyn-hickman@users.noreply.github.com>
2022-07-18 13:30:42 -0600

    starting server connections

--- api/server.js
+++ api/server.js
@@ -2,14 +2,13 @@
 // require your posts router and connect it here
 const express = require('express')
 
+const postsRouter = require('./posts/posts-router')
+
 const server = express()
 
 server.use(express.json())
 
-
-
-
-
+server.use('/api/posts', postsRouter)
 
 // CATCH ALL
 server.use('*', (req, res) => {
