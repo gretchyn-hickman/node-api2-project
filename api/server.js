5afc31acb0876a21093b7bd478c9ac8fb15104e5
Gretchyn Hickman <82916149+gretchyn-hickman@users.noreply.github.com>
2022-07-18 13:18:34 -0600

    It begins

--- api/server.js
+++ api/server.js
@@ -1,2 +1,21 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+
+
+
+
+
+
+// CATCH ALL
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'catch all Error'
+    })
+})
+
+module.exports = server
--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const server = require('./api/server')
+
+
 // require your server and launch it here
+console.log("hello")
+
+const PORT = 5000
+
+server.listen(PORT, () => {
+    console.log('listening on', PORT)
+})
