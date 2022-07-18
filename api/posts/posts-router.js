4ea0f544a8b4f9797d834d1016c285c4800f9cfb
Gretchyn Hickman <82916149+gretchyn-hickman@users.noreply.github.com>
2022-07-18 13:48:54 -0600

    setting up endpoints

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -3,4 +3,38 @@ const express = require('express')
 
 const router = express.Router()
 
+
+// GET ENDPOINTS (1, 2 & 6)
+router.get('/', (req, res) => {
+
+})
+
+router.get('/:id', (req, res) => {
+
+})
+
+router.get('/:id/messages', (req, res) => {
+
+})
+// POST ENDPOINT
+router.post('/', (req, res) => {
+
+})
+
+//PUT ENDPOINT
+router.put('/:id', (req, res) => {
+
+})
+
+//DELETE END POINT
+router.delete('/:id', (req, res) => {
+
+})
+
+
+
+
+
+
+
 module. exports = router
